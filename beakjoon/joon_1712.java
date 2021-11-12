16ccd740421e287d46d319ac4016ff532aa9d4a0
JJungEEun <79435352+JJungEEun@users.noreply.github.com>
2021-11-12 15:18:38 +0900

    Add files via upload

---
+++ beakjoon/joon_1712.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class joon_1712 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		
+		// a + b*n = n*c
+		// n = a / (c-b)
+		//손익분기점: n+1
+		
+		if (b >= c) { System.out.println(-1);}
+		else { System.out.println(a/(c-b)+1);}
+	}
+
+}
